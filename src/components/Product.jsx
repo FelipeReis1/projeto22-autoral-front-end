695fd2cee661caf7f3c2dbd700e32c7dfd4d64a8
FelipeReis1 <feliperg15@yahoo.com.br>
2023-06-19 17:06:15 -0300

    refact: product styles

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,39 +2,27 @@ import styled from "styled-components";
 
 export default function Product({ product }) {
   return (
-    <StyledProductSelection>
-      <StyledProduct>
-        <h1>{product.name}</h1>
-        <img src={product.image}></img>
-        <h1>{product.description}</h1>
-        <h1>Float: {product.itemQuality}</h1>
-        <h1>Preço: R${product.price},00</h1>
-        <h1>Categoria: {product.category}</h1>
-      </StyledProduct>
-    </StyledProductSelection>
+    <StyledProduct>
+      <h1>{product.name}</h1>
+      <img src={product.image}></img>
+      <h1>{product.description}</h1>
+      <h1>Float: {product.itemQuality}</h1>
+      <h1>Preço: R${product.price},00</h1>
+      <h1>Categoria: {product.category}</h1>
+    </StyledProduct>
   );
 }
 
-const StyledProductSelection = styled.ul`
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 10px;
-  margin-top: 20px;
-`;
-
 const StyledProduct = styled.li`
-  width: 300px;
+  width: 270px;
   height: 403px;
   box-shadow: 0px 4px 6px 4px rgba(46, 12, 126, 0.1);
   border-radius: 5px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  //justify-content: center;
   align-items: center;
-  padding: 5px;
+  padding: 3px;
   background-image: linear-gradient(to bottom, #2c2a35, #3b2632);
   h1 {
     font-size: 23px;
