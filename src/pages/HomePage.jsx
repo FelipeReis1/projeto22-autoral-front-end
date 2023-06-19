63407453e44d96c064e87b664ed9800e1f87bca2
FelipeReis1 <feliperg15@yahoo.com.br>
2023-06-19 17:06:46 -0300

    refact: product styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Product from "../components/Product";
 import styled from "styled-components";
+import Header from "../components/Header";
 export default function HomePage() {
   const [products, setProducts] = useState(undefined);
 
@@ -14,9 +15,14 @@ export default function HomePage() {
     promise.catch((error) => alert(error.response.data));
   }, []);
   return (
-    <StyledContainer>
-      {products && products.map((p) => <Product product={p} />)}
-    </StyledContainer>
+    <>
+      <Header />
+      <StyledContainer>
+        <StyledProductSelection>
+          {products && products.map((p) => <Product product={p} />)}
+        </StyledProductSelection>
+      </StyledContainer>
+    </>
   );
 }
 
@@ -25,4 +31,14 @@ const StyledContainer = styled.div`
   height: 100vh;
   display: flex;
   background-color: #111111;
+  margin-top: 64px;
+`;
+const StyledProductSelection = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-bottom: 10px;
+  margin-top: 20px;
 `;
