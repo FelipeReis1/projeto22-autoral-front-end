be0e5031bb44f49e49d24599c777695f0e7050a4
FelipeReis1 <feliperg15@yahoo.com.br>
2023-06-25 21:53:37 -0300

    feat: signin, signup, logout, home and cart buttons + functions to logout and go to cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,87 @@
 import styled from "styled-components";
 import { FaCartArrowDown } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import UserContext from "../contexts/UserContext";
+import { useContext } from "react";
+import axios from "axios";
 
 export default function Header() {
+  const { setToken, setUser, setUserId } = useContext(UserContext);
+  const name = localStorage.getItem("user");
+  const cartString = localStorage.getItem("cart");
+  const navigate = useNavigate();
+
+  function logout() {
+    if (cartString) {
+      const cart = JSON.parse(cartString);
+      const promise = axios.delete(`http://localhost:5000/cart/${cart.id}`);
+      promise.then(() => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        localStorage.removeItem("cart");
+        setToken("");
+        setUser(undefined);
+        setUserId(undefined);
+        alert("Volte sempre!");
+        navigate("/");
+      });
+    } else {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      setToken("");
+      setUser(undefined);
+      setUserId(undefined);
+      alert("Volte sempre!");
+      navigate("/");
+    }
+  }
+
+  function goToCart() {
+    const cartString = localStorage.getItem("cart");
+    if (cartString) {
+      const cart = JSON.parse(cartString);
+      console.log(cart.id);
+      navigate(`/cart/${cart.id}`);
+    } else {
+      alert("Adicione produtos para visualizar o carrinho!");
+    }
+  }
+
   return (
     <StyledHeader>
-      <StyledLogin>Login</StyledLogin>
-      <StyledLogout>Logout</StyledLogout>
-      <StyledCart>
+      <>
+        {name ? (
+          <h1>Bem vindo(a), {name}!</h1>
+        ) : (
+          <h1>Bem vindo(a), CSGO Player!</h1>
+        )}
+      </>
+      <StyledButton>
+        <Link to={"/"} style={{ textDecoration: "none" }}>
+          <h1>Home</h1>
+        </Link>
+      </StyledButton>
+      {name ? (
+        <></>
+      ) : (
+        <StyledButton>
+          <Link to={"/signin"} style={{ textDecoration: "none" }}>
+            <h1>Sign-in</h1>
+          </Link>
+        </StyledButton>
+      )}
+      {name ? (
+        <></>
+      ) : (
+        <StyledButton>
+          <Link to={"/signup"} style={{ textDecoration: "none" }}>
+            <h1>Sign-up</h1>
+          </Link>
+        </StyledButton>
+      )}
+
+      <StyledButton onClick={logout}>Logout</StyledButton>
+      <StyledCart onClick={goToCart}>
         <FaCartArrowDown />
       </StyledCart>
     </StyledHeader>
@@ -23,22 +98,32 @@ const StyledHeader = styled.div`
   font-size: 22px;
   background-color: #1d1818;
   position: fixed;
+  font-family: "Ubuntu", sans-serif;
+  font-style: normal;
   top: 0;
   left: 0;
+  h1 {
+    color: white;
+  }
 `;
 
-const StyledLogin = styled.button`
-  width: 50px;
+const StyledButton = styled.button`
+  width: 85px;
   height: 30px;
-`;
-
-const StyledLogout = styled.button`
-  width: 60px;
-  height: 30px;
+  border-radius: 5px;
+  background-color: transparent;
+  font-weight: bold;
+  color: white;
+  :hover {
+    cursor: pointer;
+  }
 `;
 
 const StyledCart = styled(FaCartArrowDown)`
   width: 50px;
   height: 25px;
   fill: white;
+  :hover {
+    cursor: pointer;
+  }
 `;
